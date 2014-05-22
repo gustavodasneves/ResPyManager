1d272dcb8a935dce2ec8c2a716eaba3783aafb72
Gustavo das Neves <gustavo.nevesgn@gmail.com>
2014-05-22 00:50:57 -0300

    Inclusão de arquivos alterados e teste para merge

--- front/app/scripts/app.js
+++ front/app/scripts/app.js
@@ -12,6 +12,10 @@ angular.module('respyManagerApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/dashboard', {
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
