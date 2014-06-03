62e404d0d8cb85610d15229a0935da608e96d362
eduardo <eduardovalimlima@gmail.com>
2014-06-02 21:10:56 -0300

    first commit

--- respy/admin.py
+++ respy/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from respy.models import Agendamento, Turno, HorarioPeriodo
+
+class TurnoAdmin(admin.ModelAdmin):
+    fields = ('nome', 'inicio',)
+    list_display = ('turno_id', 'nome', 'inicio', )
+
+admin.site.register(Agendamento)
+admin.site.register(Turno, TurnoAdmin)
+admin.site.register(HorarioPeriodo)
