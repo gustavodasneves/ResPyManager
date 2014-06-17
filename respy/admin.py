9c1621c5c6c19924987cb714169a6fbf5033b63e
eduardo <eduardovalimlima@gmail.com>
2014-06-16 21:17:02 -0300

    first commit

--- respy/admin.py
+++ respy/admin.py
@@ -5,19 +5,19 @@ from respy.models import Local,Equipamento,Aluguel
 
 
 class LocalAdmin(admin.ModelAdmin):
-    fields = ('local_id','nome')
+    fields = ('nome',)
     list_display = ('local_id','nome')
 
 class EquipamentoAdmin(admin.ModelAdmin):
-    fields = ('equipamento_id','nome')
+    fields = ('nome',)
     list_display = ('equipamento_id','nome')
 
 class AluguelAdmin(admin.ModelAdmin):
-    fields = ('aluguel_id','local_id','equipamento_id','user_id','datahorainicio','datahorafim','ativo')
+    fields = ('local_id','equipamento_id','user_id','datahorainicio','datahorafim','ativo',)
     list_display = ('aluguel_id','local_id','equipamento_id','user_id','datahorainicio','datahorafim','ativo')
 
 
-admin.site.register(Local,LocalAdmin )
+admin.site.register(Local,LocalAdmin)
 admin.site.register(Equipamento, EquipamentoAdmin)
 admin.site.register(Aluguel,AluguelAdmin)
 
--- respy/models.py
+++ respy/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Local (models.Model):
     local_id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=100)
+
     def __unicode__(self):
         return self.nome
 
