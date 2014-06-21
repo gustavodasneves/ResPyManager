61f32ccba3b2d463b254bd7df6184f28052cb815
Murilo Mittmann <mmittmann@outlook.com>
2014-06-21 01:11:46 -0300

    Iniciado processo de retornar dados via JSON

--- respy/views.py
+++ respy/views.py
@@ -1,3 +1,13 @@
+from django.core import serializers
 from django.shortcuts import render
+from respy.models import Equipamento
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def equipamentos(request):
+    data = {}
+    lista_equipamentos = Equipamento.objects.all()
+    data = serializers.serialize("json", lista_equipamentos)
+    return HttpResponse(data, content_type = "application/json")
